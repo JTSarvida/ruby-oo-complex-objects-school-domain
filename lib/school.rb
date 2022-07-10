d8b91c8411a26b047c23abae1a8518d1593ba3c0
jtsarvida <jtsarvida@gmail.com>
2022-07-10 04:38:51 -0400

    Completed Assignment

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+
+    attr_accessor :grade, :roster
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name   
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.map {|grades, students| @roster[grades] = students.sort}
+        @roster
+    end
+end
